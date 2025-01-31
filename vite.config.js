de802f3c0fcc0ae4bde5a5477b1e208595a40260
Gerardo Lucero <23587014+GerardoLucero@users.noreply.github.com>
2025-01-30 21:41:33 -0600

    feat: assest img

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lawyers-site/' // ✅ Ajusta esto con el nombre de tu repositorio
-})
+  base: '/lawyers-site/',  
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',  // Asegura que las imágenes y otros assets se copien correctamente
+  }
+});
