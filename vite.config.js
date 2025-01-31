6823b64122c1034c8b3a164688e7b43f7058834a
Gerardo Lucero <23587014+GerardoLucero@users.noreply.github.com>
2025-01-30 20:53:55 -0600

    feat: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/lawyers-site/' // Nombre del repositorio en GitHub
 })
